87bf26dd963cb445a09945843f3cc5b9ff37f3a9
Joseph Hassell <jedhassell@gmail.com>
2023-11-08 13:18:37 -0800

    updating

--- build_image_yml.rb
+++ build_image_yml.rb
@@ -1,11 +1,11 @@
 require 'yaml'
 
-dir = '/Users/joseplha/projects/jed/photorama/img/galleries/g01/'
-prefix = '/img/galleries/g01/'
+dir = '/Users/joseplha/projects/jed/photorama/img/galleries/g02/'
+prefix = '/img/galleries/g02/'
 result = {}
 Dir.foreach(dir) do |filename|
   result['images'] ||= []
-  result['images'] << {'image_path' => "#{prefix}#{filename}", 'caption' => 'Ring Mountain', 'copyright' => '© Jed Hassell'} if filename.end_with?('.jpg')
+  result['images'] << {'image_path' => "#{prefix}#{filename}", 'caption' => 'Headlands', 'copyright' => '© Jed Hassell'} if filename.end_with?('.jpeg')
 end
 
 puts result.to_yaml
